38d0a0dac3d77b7a56c0dd4983b8f52c92b538ed
Enmanuelle Martinez <98730886+EMLDeveloper@users.noreply.github.com>
2022-03-22 16:20:56 -0700

    Parte del Proyecto

--- src/library/system/System.java
+++ src/library/system/System.java
@@ -1,10 +1,36 @@
 
 package library.system;
 
-/**
- *
- * @author Usuario
- */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class System {
+    private static Connection conn;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String pass = "123456";
+    private static final String db = "library";
+    private static final String url = "jdbc:mysql://localhost/"+db+"";
     
+    public System(){
+        conn = null;
+        try{
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, pass);
+            
+            // Connect?
+            if(conn != null)
+                System.out.println("Conexión establecida exitosamente");
+        }catch (ClassNotFoundException | SQLException ex){
+            System.out.println("Conexión Fallida:\n\n"+ex);
+        }
+    }
+    
+
+    public Connection getConnection() {
+        return conn; //To change body of generated methods, choose Tools | Templates.
+    }
 }
+
