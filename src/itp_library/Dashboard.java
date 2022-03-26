cd85c69227935e8be22fec48ea4e9b7aa8d09f8b
areche10 <brianareche2002@gmail.com>
2022-03-26 02:15:23 +0100

    Merge pull request #1 from EMLDeveloper/master
    update

--- src/itp_library/Dashboard.java
+++ src/itp_library/Dashboard.java
@@ -628,6 +628,7 @@ public class Dashboard extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
